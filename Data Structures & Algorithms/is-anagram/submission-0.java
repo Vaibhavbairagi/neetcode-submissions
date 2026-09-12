107e046999e84946c3e56a44550ee739c7139622
Vaibhav Bairagi <vaibhavbairagi105@gmail.com>
2026-09-13 03:47:36 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[128];
+        for(int i = 0;i<s.length();i++) {
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
+        }
+        for(int i = 97;i<123;i++) {
+            if(count[i]!=0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
